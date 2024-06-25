cd17d3b9f661d6672c1985f7b23b169415da8bc8
Will Ley <wley3337@gmail.com>
2024-06-25 17:20:49 -0400

    basic api outline

---
+++ go/go_bank/api.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// write our response to json
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+// wrapper to allow for handling of errors acts as a decorator
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			// handle error here
+		}
+	}
+}
+
+type APIServer struct {
+	listenAddr string
+}
+
+func NewAPIServer(listenAddr string) *APIServer {
+	return &APIServer{
+		listenAddr: listenAddr,
+	}
+}
+
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+	router.HandleFunc("/accounts", s.handleAccount)
+}
+
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
---
+++ go/go_bank/go.mod
@@ -0,0 +1,5 @@
+module github.com/wley3337/learning/tree/main/go/go_bank
+
+go 1.22.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
