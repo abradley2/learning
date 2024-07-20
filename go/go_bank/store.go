d9ff60252ce38cefbc93ab80f136b244afa3965d
Will Ley <wley3337@gmail.com>
2024-07-20 15:31:02 -0400

    first bit of getting a Postgres docker server connected and creating db table

---
+++ go/go_bank/store.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+
+type Storage interface {
+	CreateAccount(*Account) error
+	GetAccount(int) (*Account, error)
+	UpdateAccount(*Account) error
+	DeleteAccount(int) error
+}
+
+type PostgresStore struct {
+	db *sql.DB
+}
+
+/**
+ * currently requires you to run: `docker run --name go_bank_db -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=go_bank_db -p 5432:5432 -d postgres`
+ */
+func NewPostgresStore() (*PostgresStore, error) {
+	// connStr := "user=postgres dbname=go_bank_db password=postgres sslmode=verify-full"
+	connStr := "user=postgres dbname=go_bank_db password=postgres sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &PostgresStore{db: db}, nil
+}
+
+func (s *PostgresStore) Init() error {
+
+	return s.createAccountTable()
+}
+
+// region DB Migrations
+func (s *PostgresStore) createAccountTable() error {
+	query := `CREATE TABLE IF NOT EXISTS account
+    (
+    id SERIAL PRIMARY KEY,
+    first_name BPCHAR NOT NULL,
+    last_name BPCHAR NOT NULL,
+    account_number UUID UNIQUE,
+    balance DECIMAL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+    )`
+
+	_, err := s.db.Exec(query)
+
+	return err
+}
+
+// endregion
+func (s *PostgresStore) CreateAccount(*Account) error {
+	return nil
+}
+func (s *PostgresStore) GetAccount(int) (*Account, error) {
+	return nil, nil
+}
+func (s *PostgresStore) UpdateAccount(*Account) error {
+	return nil
+}
+func (s *PostgresStore) DeleteAccount(int) error {
+	return nil
+}
--- go/go_bank/types.go
+++ go/go_bank/types.go
@@ -7,18 +7,18 @@ import (
 )
 
 type Account struct {
-	ID        int       `json:"id"`
-	FirstName string    `json:"firstName"`
-	LastName  string    `json:"lastName"`
-	Number    uuid.UUID `json:"number"`
-	Balance   int64     `json:"balance"`
+	ID            int       `json:"id"`
+	FirstName     string    `json:"firstName"`
+	LastName      string    `json:"lastName"`
+	AccountNumber uuid.UUID `json:"accountNumber"`
+	Balance       int64     `json:"balance"`
 }
 
 func NewAccount(firstName, lastName string) *Account {
 	return &Account{
-		ID:        rand.Intn(10000),
-		FirstName: firstName,
-		LastName:  lastName,
-		Number:    uuid.New(),
+		ID:            rand.Intn(10000),
+		FirstName:     firstName,
+		LastName:      lastName,
+		AccountNumber: uuid.New(),
 	}
 }
